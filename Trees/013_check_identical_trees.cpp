e3f16072c2610ec9d445db8eeba7735033c295c7
Sandeep Kolla <58244062+sandeep-2601@users.noreply.github.com>
2023-01-12 14:31:24 +0530

    Update 013_check_identical_trees.cpp

--- Trees/013_check_identical_trees.cpp
+++ Trees/013_check_identical_trees.cpp
@@ -14,6 +14,14 @@ struct Node {
         this->right = right;
     }
 };
+
+bool isSameTree(TreeNode* p, TreeNode* q) {
+       if(p == NULL && q == NULL) return true;
+       else if(p == NULL) return false;
+       else if(q == NULL) return false;
+       return (p->val == q->val && isSameTree(p->left,q->left) && isSameTree(p->right,q->right));
+}
+
 int main() {
 
     Node* root   = new Node(1);
